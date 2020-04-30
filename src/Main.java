0db4393893409d7186ade89b2dbcf231f0c35804
Daniel-Kao <396007476@qq.com>
2020-04-30 22:06:55 +0800

    ha

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = (int) Math.floor(1.1f);
+        System.out.println("Hello World!");
+        System.out.println(a);
+    }
+}
